e5a398da2aa70896475aa6c4a2db49555854340e
Hector Cald <hi.hector20@gmail.com>
2025-08-28 16:05:16 -0400

    conexion backend y frontend

--- src/services/userService.js
+++ src/services/userService.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = 'http://localhost:5000/api';
+const API_BASE_URL = process.env.REACT_APP_API_URL;
 
 class UserService {
   // Función para guardar el token en localStorage y cookies
