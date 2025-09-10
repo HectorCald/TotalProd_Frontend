f4b55be044d003ed3e7fe561738a07e67e0bfa7b
Hector Cald <hi.hector20@gmail.com>
2025-09-10 12:24:29 -0400

    nuevo pedido acopio y movimientos

--- src/components/common/PantallaExito.jsx
+++ src/components/common/PantallaExito.jsx
@@ -33,7 +33,7 @@ function PantallaExito({
                 setMostrarCheck(true);
                 setTimeout(() => {
                     setMostrarSpinner(false);
-                }, 500);
+                }, 1000);
             }, 1000);
 
             return () => clearTimeout(timer);
