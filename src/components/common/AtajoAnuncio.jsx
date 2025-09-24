3f20e5f547c379a9a8c2099dafc998cebbc21d0c
Hector Cald <hi.hector20@gmail.com>
2025-09-24 10:38:00 -0400

    Elimine capacitor

--- src/components/common/AtajoAnuncio.jsx
+++ src/components/common/AtajoAnuncio.jsx
@@ -3,7 +3,7 @@ import styles from './AtajoAnuncio.module.css';
 function AtajoAnuncio({ title, description, image, onClick }) {
     return (
         <div className={styles.atajoAnuncio} onClick={onClick}>
-            <img src={image} alt={title} />
+            <img src={image} alt={title} className={styles.image} />
             <div className={styles.content}>
                 <h3 className={styles.title}>{title}</h3>
                 <p className={styles.description}>{description}</p>
