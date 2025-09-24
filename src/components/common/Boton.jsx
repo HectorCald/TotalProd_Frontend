099166fcf7158dc2c57839d85b4814b7e4984b09
Hector Cald <hi.hector20@gmail.com>
2025-09-24 16:35:23 -0400

    Correciones y cierre de sesion directo

--- src/components/common/Boton.jsx
+++ src/components/common/Boton.jsx
@@ -3,7 +3,8 @@ import styles from './Boton.module.css';
 
 function Boton({ label, onClick, className, icon, loading, disabled, style='', objeto}) {
   return (
-    <button className={`${styles.btn} ${styles[className] } ${loading ? styles.loading : ''} ${disabled ? styles.disabledButton : ''}`} onClick={onClick} style={{...style}}>
+    <button className={`${styles.btn} ${styles[className] } ${loading ? styles.loading : ''} ${disabled ? styles.disabledButton : ''}`} onClick={loading ? null : onClick} style={{...style}}>
+      {loading && <div className={styles.overlay}></div>}
       {icon && <img src={icon} alt="icon" className={styles.icon}/>}
       {objeto && <span className={styles.objeto} >{objeto}</span>}
       
