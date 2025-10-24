04db9249d094d25d6b51f22a9e4e990dc52ba2c9
Hector Cald <hi.hector20@gmail.com>
2025-10-24 09:45:00 -0400

    Correcion de errores y diseño (plan no activo de empelado)

--- src/config/api.js
+++ src/config/api.js
@@ -1,7 +1,7 @@
 // Configuración central de la API
 const API_CONFIG = {
     // Cambia aquí para cambiar entre local y producción
-    ENVIRONMENT: 'local', // 'local' | 'production
+    ENVIRONMENT: 'production', // 'local' | 'production
     
     // URLs de los backends
     LOCAL: 'http://localhost:5000/api',
