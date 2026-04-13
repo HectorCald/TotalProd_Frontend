12b9f69b4c5dd0b90030f509abf965c3c3ac4fee
Hector Cald <hi.hector20@gmail.com>
2026-04-13 18:26:09 -0400

    Filtrado de fechas corregidas

--- src/constants/detallesUpdate.js
+++ src/constants/detallesUpdate.js
@@ -3,39 +3,11 @@ export const detallesUpdate = [
         label: 'Novedades',
         children: [
             {
-                label: 'Movimientos de Almacen',
+                label: 'Corrección de error en el modal de filtardo de fechas.',
                 children: [
-                    'Se optimizo el panel de movimientos de almacen y materia prima.'
+                    'Se corrigio el error que no mostraba los productos en las cotizaciones.'
                 ]
             },
-            {
-                label: 'Reportes',
-                children: [
-                    'Se optimizo el panel de reportes.',
-                    'Se agregaron filtros de fecha para los reportes.'
-                ]
-            },
-            {
-                label: 'Diseño y experiencia de usuario',
-                children: [
-                    'Actualización de diseño y experiencia de usuario en todas las pantallas.'
-                ]
-            },
-            {
-                label: 'Corrección de Errores',
-                children: [
-                    'Se corrigieron errores en el formulario de producción.',
-                    'Se corrigieron los reportes de gastos, deudas, ventas, materia prima y almacen.',
-                    'Se elimaron los reportes innecesarios.',
-                    'Se borro la funcionalidad de modo Offline.',
-                    'Se quitaron filtros innecesarios.',
-                    'Se corrigio el bug que no mostraba los productos en las cotizaciones.',
-                    'Se Agregaron nuevas columnas necesarias en algunos modulos.',
-                    'Se obliga a conceder ubicación para mejora de la seguridad y funcionalidad de la aplicación.',
-                    'Ajustes de errores en diferentes módulos de la aplicación.',
-                    'Mejora del manejo de errores para una mejor retroalimentación al usuario.'
-                ]
-            }
         ]
     }
 ];
